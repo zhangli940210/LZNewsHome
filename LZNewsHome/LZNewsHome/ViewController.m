52d71d33dc6a5e23f46d78fc556a410dd66096e6
gj <gj@itcast.cn>
2016-03-25 09:11:50 +0800

    初始化项目

---
+++ LZNewsHome/LZNewsHome/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LZNewsHome
+//
+//  Created by apple on 16/3/25.
+//  Copyright © 2016年 m14a.cn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+@end
